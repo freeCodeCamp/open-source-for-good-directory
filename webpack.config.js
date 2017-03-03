1d0b76810230e4e7b29b0d35eab3a257f08cca25
Joe McIlhargey <jmcilhargey@gmail.com>
2017-03-03 00:04:23 -0800

    Initial dependencies and updating some linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+const path = require('path');
+const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-  template: __dirname + '/src/index.html',
+  template: path.join(__dirname, '/src/index.html'),
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
+});
+const DefinePluginConfig = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
 });
 
 module.exports = {
@@ -10,25 +16,25 @@ module.exports = {
   {
     'app': [
       'react-hot-loader/patch',
-      __dirname + '/src/index.js'
-    ]
+      path.join(__dirname, '/src/index.js'),
+    ],
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ]
+        loader: 'style-loader!css-loader',
+      },
+    ],
   },
   output: {
     filename: 'index.js',
-    path: __dirname + '/build'
+    path: path.join(__dirname, '/build'),
   },
-  plugins: [ HTMLWebpackPluginConfig ]
+  plugins: [HTMLWebpackPluginConfig, DefinePluginConfig],
 };
