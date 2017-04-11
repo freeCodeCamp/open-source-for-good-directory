74408c3116ef909161862221a0c1425081262ebc
Joe McIlhargey <jmcilhargey@gmail.com>
2017-04-11 11:21:35 -0700

    Added as-seen-on img and text

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 const Banner = () => (
   <div>
     <h1 className="main-title">Open Source for Good</h1>
-    <h3 className="main-desc">Intro</h3>
+    <h3 className="main-desc">Our mission is to provide free high-quality software to non-profits</h3>
     <div className="as-seen-on">
-      <p>As seen on</p>
+      <h3 className="as-seen-on-desc">As seen on...</h3>
+      <img
+        src="https://s3.amazonaws.com/freecodecamp/as-seen-on.png"
+        alt="freeCodeCamp has been featured on these publications"
+      />
     </div>
     <hr className="break" />
   </div>
