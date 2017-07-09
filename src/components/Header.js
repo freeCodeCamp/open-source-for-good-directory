1a3c279c3db3df33e87bb645c236d20bc884ff4a
Juan D. Acosta <juandacorias@gmail.com>
2017-07-09 10:10:28 -0500

    Marketing phrase fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,21 +12,12 @@ const Header = () =>
     </a>
     <ul className='header-links'>
       <li>
-        <a href='https://www.freecodecamp.org/donate' target='_blank'>
-          Donate
-        </a>
+        <a href='https://www.freecodecamp.org/donate' target='_blank'>Donate</a>
       </li>
       <li>
         <a href='https://www.freecodecamp.org/'>Return to freeCodeCamp.org</a>
       </li>
     </ul>
-    <a
-      href='https://www.freecodecamp.org/donate'
-      id='donate-icon'
-      target='_blank'
-      >
-      <i aria-hidden='true' className='fa fa-heart' />
-    </a>
   </nav>;
 
 export default Header;
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ const Title = () =>
   <div>
     <h1 className='main-title'>Open Source for Good</h1>
     <h3 className='main-desc'>
-      "Spend money on the mission, not the software."
+      Spend money on the mission, not the software.
     </h3>
   </div>;
 
