1abaae43e90449b5e6fad8cba6bde00d73433b19
Juan D. Acosta <juandacorias@gmail.com>
2017-07-11 17:25:55 -0500

    Testing Server Capitalization modification

--- .osfg-dir-config.js
+++ .osfg-dir-config.js
@@ -1,16 +1,16 @@
 /*
   OPEN SOURCE FOR GOOD DIRECTORY SAMPLE CONFIG FILE 
 
-  title: Displayed at the Top and as the WebPage Title
-  demoVideo: for the moment it only works with a regular YouTube links.
-  liveDemo: address to the Homepage of the Project.
-  description: an ES6 Template String with Github Markdown.
-               Keep it relatively short, a paragraph tops.
-  body: an ES6 Template String with Github Markdown. 
-        Please include the license and FreeCodeCamp copyright at the end.
+  title: Displayed at the top and as the webpage title
+  demoVideo: the YouTube video address
+  liveDemo: the address of the project's homepage
+  description: an ES6 template string that contains GitHub-flavored Markdown. 
+               Keep this relatively short - a paragraph tops
+  body: an ES6 template string with GitHub-flavored Markdown. 
+        Please include the open source license, with the freeCodeCamp copyright at the end.
 
-  If the 'liveDemo' or the 'demoVideo' aren't yet available you can 
-  exclued them, they just won't be added to the Project's page.
+  If the 'liveDemo' or the 'demoVideo' aren't yet available, you can exclued them.
+  They just won't be added to the project's page.
 */
 
 module.exports = {
